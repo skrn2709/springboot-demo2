d8d34a1ce82b70f5001e84356897c97f665d89c5
saikiranmuthyala2709.01@gmail.com <saikiranmuthyala2709.01@gmail.com>
2021-04-19 18:19:27 +0530

    @CrossOrigin

--- student-demo/src/main/java/com/studentmgmt/demo/controller/CovidApiController.java
+++ student-demo/src/main/java/com/studentmgmt/demo/controller/CovidApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.studentmgmt.demo.model.CovidRow;
 
 @RestController
 @RequestMapping(path = "/covidapi")
+@CrossOrigin
 public class CovidApiController {
 
 	@GetMapping(path = "/getData")
