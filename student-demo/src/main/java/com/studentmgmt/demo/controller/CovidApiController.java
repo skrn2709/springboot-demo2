6a1373093b596be8b22eece730b34592b4f906c6
saikiranmuthyala2709.01@gmail.com <saikiranmuthyala2709.01@gmail.com>
2021-04-19 18:17:43 +0530

    responseentity

--- student-demo/src/main/java/com/studentmgmt/demo/controller/CovidApiController.java
+++ student-demo/src/main/java/com/studentmgmt/demo/controller/CovidApiController.java
@@ -3,7 +3,10 @@ package com.studentmgmt.demo.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,20 @@ import com.studentmgmt.demo.model.CovidRow;
 public class CovidApiController {
 
 	@GetMapping(path = "/getData")
-	public List<CovidRow> getData(){
+	public ResponseEntity<List<CovidRow>> getData(){
+		List<CovidRow> data=null;
+		data=Arrays.asList(
+				new CovidRow("Narayana","19/04 Night",97),
+				new CovidRow("Bhagya","19/04 Night",98),
+				new CovidRow("Kiran","19/04 Night",97)
+				);
+		ResponseEntity<List<CovidRow>> response=
+				new ResponseEntity<List<CovidRow>>(data, HttpStatus.OK);
+		return response;
+	}
+	
+	@PostMapping(path = "/addrow")
+	public List<CovidRow> addrow(){
 		List<CovidRow> data=null;
 		data=Arrays.asList(
 				new CovidRow("Narayana","19/04 Night",97),
